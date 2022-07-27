aca2b02e298ac46b4575055a04e0238151daf65e
stheoliveira <sthephanyoliiveira.f@gmail.com>
2022-07-27 17:13:56 -0300

    iniciando_js

---
+++ Introdução a criação de websites com HTML5 e CSS3/Dominando a linguagem JavaScript/teste.js
@@ -0,0 +1,14 @@
+// comentário de uma linha
+/* comentário
+com maisd e uma linha */
+
+var desconto = .;
+const PRECO = ;
+var total = PRECO - desconto;
+//constante sempre com letra maiúscula; boa prática
+
+function soma(a, b) {
+    return a + b; //8
+}
+
+soma (3,5);
