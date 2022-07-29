d3436f756d61e96c000070ad5ea4309713065fa7
stheoliveira <sthephanyoliiveira.f@gmail.com>
2022-07-28 22:05:01 -0300

    variaveis

--- Dominando a linguagem JavaScript/Sintaxe básica em JS/script.js
+++ Dominando a linguagem JavaScript/Sintaxe básica em JS/script.js
@@ -1,1 +1,97 @@
 alert('tudo certo')
+
+//tipos primitivos
+
+//booleanos 
+/* var vOuF = false;
+console.log(typeof(vOuF));
+
+// number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+// string 
+var nome = 'sthe';
+console.log (typeof(nome));
+
+// function 
+var funcao = function () {}
+console.log(typeof(funcao)); */
+
+// como declarar variavel
+/* var variavel = 'sthe';
+variavel = 'oliveira';
+console.log(variavel);
+
+let variavel2 = 'sthe';
+variavel2 = 'oliveira';
+console.log(variavel); 
+
+const constante = 'sthe';
+console.log(constante); */
+
+/* var escopoGlobal = 'global';
+console.log(escopoGlobal);
+
+function escopoLocal() {
+    let escopoLocalInterno = 'local';
+    console.log(escopoLocalInterno);
+}
+
+escopoLocal(); */
+
+// atribuicao
+/* var atribuicao = 'sthe';
+
+// comparacao
+var comparacao = '0' == 0;
+console.log(comparacao)
+
+// comparacao identica
+var comparacaoIdentica =  '0' === 0;
+console.log(comparacaoIdentica); */
+
+/* // adicao
+var adicao = 1 + 1;
+console.log(adicao);
+
+// subtracao
+var subtracao = 2 - 1;
+console.log(subtracao);
+
+// multiplicacao
+var multiplicacao = 2 * 3;
+console.log(multiplicacao);
+
+// divisao real
+var divisaoReal = 6 / 2;
+console.log(divisaoReal);
+
+// potenciacao
+var potenciacao = 2 ** 10;
+console.log(potenciacao); */
+
+// maior que
+/*var maiorQue = 5 > 2;
+console.log(maiorQue);
+
+// menor que
+var menorQue = 5 < 2;
+console.log(menorQue);
+
+// maior ou igual a
+var maiorOuIgual =  5 > 2;
+console.log(maiorOuIgual);
+
+// menor ou igual a 
+var menorOuIgual = 5 < 2;
+console.log(menorOuIgual); */
+
+var e = true && true;
+console.log(ou);
+
+var ou = true || false;
+console.log(ou);
+
+var nao = !true;
+console.log(false);
