dd4d41b1ddc1522f31ddb935a02498b3325c137e
stheoliveira <sthephanyoliiveira.f@gmail.com>
2022-07-30 00:15:08 -0300

    funções

---
+++ Dominando a linguagem JavaScript/Sintaxe básica em JS/funções/script.js
@@ -0,0 +1,11 @@
+/* function funcao() {
+    console.log('amem, deu certo?')
+}
+
+funcao(); */
+
+function mensagem(primeiro, segundo) {
+    console.log(primeiro, segundo)
+}
+
+mensagem('amem', 'deu certo?');
