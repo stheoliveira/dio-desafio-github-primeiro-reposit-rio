e0898a12fedd025edd2143249f3321b19115aaf5
stheoliveira <sthephanyoliiveira.f@gmail.com>
2022-08-16 23:12:24 -0300

    att

---
+++ Dominando a linguagem JavaScript/Variáveis e tipos/Atribuindo valores/tipos.js
@@ -0,0 +1,26 @@
+//Strings
+let nome = "João"
+let sobrenome = "Pedro"
+
+typeof nome;
+
+nome.concat(sobrenome);
+
+let concatenado = nome.concat(sobrenome)
+
+typeof concatenado
+
+concatenado.length;
+
+let exemplo = new String("qualquercoisa")
+
+typeof exemplo
+
+exemplo.length
+
+let vazio = ""
+
+nome
+
+nome.length
+
